9ff7a80be8d2b4fcdcd549db451434dbe94230f4
pangr <pangr@umich.edu>
2020-02-03 23:00:39 -0500

    json, python addition

---
+++ projects.py
@@ -0,0 +1,19 @@
+data = {
+    "data": [
+        {
+        "id": 1,
+        "Title": "Instagram, Remade",
+        "Description": "Remade Instagram with client side and server side dynamic \
+            pages.",
+        "Tech Stack": ["Flask", "MySQL", "React.js"],
+        "Demo Link": "https://www.youtube.com/watch?v=d_IBDNTvAfs",
+        "Source Code": ""
+        }
+    ]
+}
+
+import json
+if __name__ == "__main__":
+    with open('projects.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
