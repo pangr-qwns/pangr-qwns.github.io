da45807b9c95cdacb931bea6453355e44bacb469
pangr <pangr@umich.edu>
2020-02-04 13:01:32 -0500

    added about, photography page

--- projects.py
+++ projects.py
@@ -1,21 +1,37 @@
 data = {
     "data": [
         {
-        "id": 1,
-        "Title": "Instagram, Remade",
-        "Description": "Remade Instagram with client side and server side dynamic "
-                       "pages.",
-        "Skills": ["Flask", "MySQL", "React.js"],
-        "Demo": "https://www.youtube.com/watch?v=d_IBDNTvAfs",
-        "Source": ""
+            "id": 1,
+            "Title": "Instagram, Remade",
+            "Description": "Remade Instagram with client side and server side dynamic "
+                           "pages.",
+            "Skills": ["Flask", "MySQL", "React.js"],
+            "Demo": "https://www.youtube.com/watch?v=d_IBDNTvAfs",
+            "Source": ""
         },
         {
-        "id": 2,
-        "Title": "Lorem ipsum",
-        "Description": "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.",
-        "Skills": ["Lorem ipsum", "Lorem ipsum", "Lorem ipsum"],
-        "Demo": "Lorem ipsum",
-        "Source": "Lorem ipsum"
+            "id": 2,
+            "Title": "Using Data Mining to Predict Suicide Rates",
+            "Description": "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.",
+            "Skills": ["R", "Linear Regression", "Splines", "GAM"],
+            "Demo": "Lorem ipsum",
+            "Source": "Lorem ipsum"
+        },
+        {
+            "id": 3,
+            "Title": "Interactive RFID",
+            "Description": "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.",
+            "Skills": ["Python"],
+            "Demo": "Lorem ipsum",
+            "Source": "Lorem ipsum"
+        },
+        {
+            "id": 4,
+            "Title": "Mobile Barcode Scanner",
+            "Description": "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.",
+            "Skills": ["Product Design"],
+            "Demo": "Lorem ipsum",
+            "Source": "Lorem ipsum"
         }
     ]
 }
