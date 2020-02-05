fcd0328aaca04bcbdb11ccb8d44f75a4ca65671b
pangr <pangr@umich.edu>
2020-02-04 19:03:57 -0500

    update

--- projects.py
+++ projects.py
@@ -1,37 +1,41 @@
 data = {
     "data": [
         {
-            "id": 1,
-            "Title": "Instagram, Remade",
-            "Description": "Remade Instagram with client side and server side dynamic "
-                           "pages.",
-            "Skills": ["Flask", "MySQL", "React.js"],
-            "Demo": "https://www.youtube.com/watch?v=d_IBDNTvAfs",
-            "Source": ""
-        },
-        {
-            "id": 2,
-            "Title": "Using Data Mining to Predict Suicide Rates",
-            "Description": "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.",
-            "Skills": ["R", "Linear Regression", "Splines", "GAM"],
-            "Demo": "Lorem ipsum",
-            "Source": "Lorem ipsum"
-        },
-        {
-            "id": 3,
-            "Title": "Interactive RFID",
-            "Description": "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.",
-            "Skills": ["Python"],
-            "Demo": "Lorem ipsum",
-            "Source": "Lorem ipsum"
-        },
-        {
-            "id": 4,
             "Title": "Mobile Barcode Scanner",
             "Description": "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.",
             "Skills": ["Product Design"],
             "Demo": "Lorem ipsum",
-            "Source": "Lorem ipsum"
+            "Source": "Lorem ipsum",
+            "Image": "./static/instagram.jpg"
+        },
+        {
+            "Title": "Using Data Mining to Predict Suicide Rates",
+            "Description": "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.",
+            "Skills": ["R", "Linear Regression", "Splines", "GAM"],
+            "Demo": "Lorem ipsum",
+            "Source": "Lorem ipsum",
+            "Image": "./static/instagram.jpg"
+        },
+        {
+            "Title": "Interactive RFID",
+            "Description": "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.",
+            "Skills": ["Python"],
+            "Demo": "Lorem ipsum",
+            "Source": "Lorem ipsum",
+            "Image": "./static/instagram.jpg"
+        },
+        {
+            "Title": "Insta485",
+            "Description": "My Web Systems class, EECS 485, had me recreating "
+                           "Instagram. I implemented my own REST API to "
+                           "handle database requests for comments, photos, "
+                           "likes, and all the other good stuff in Instagram. "
+                           "Unfortunately, the honor code does not let me "
+                           "post my project code. But enjoy the demo!",
+            "Skills": ["Flask", "MySQL", "React.js"],
+            "Demo": "https://www.youtube.com/watch?v=d_IBDNTvAfs",
+            "Source": "",
+            "Image": "./static/instagram.jpg"
         }
     ]
 }
