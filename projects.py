2ed3e04503fd20dfe56eb43dc8d69f7e21e4c937
pangr <pangr@umich.edu>
2020-02-04 20:21:06 -0500

    finished projects page except for pictures

--- projects.py
+++ projects.py
@@ -2,26 +2,38 @@ data = {
     "data": [
         {
             "Title": "Mobile Barcode Scanner",
-            "Description": "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.",
-            "Skills": ["Product Design"],
-            "Demo": "Lorem ipsum",
-            "Source": "Lorem ipsum",
+            "Description": "I attended a hackathon in May 2018 and built a "
+                           "shopping app. Users could scan item "
+                           "barcodes and the item would appear in the user's "
+                           "cart. I drafted wireframes and developed the "
+                           "front end parts of the app.",
+            "Skills": ["HTML/CSS", "Javascript", "Wireframing"],
+            "Demo": "https://www.youtube.com/watch?v=kTKF-1_lmnc",
+            "Source": "https://github.com/pangr-qwns/waitless",
             "Image": "./static/instagram.jpg"
         },
         {
             "Title": "Using Data Mining to Predict Suicide Rates",
-            "Description": "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.",
+            "Description": "Working with two other data scientists, I ran a "
+                           "dataset of suicide information on different data "
+                           "analysis models to predict what factors "
+                           "contributed to suicide rates.",
             "Skills": ["R", "Linear Regression", "Splines", "GAM"],
-            "Demo": "Lorem ipsum",
-            "Source": "Lorem ipsum",
+            "Demo": "./static/suicide_report.pdf",
+            "Source": "https://github.com/pangr-qwns/predictingSuicideRates/blob/master/Predicting%20Suicide%20Rates.pdf",
             "Image": "./static/instagram.jpg"
         },
         {
             "Title": "Interactive RFID",
-            "Description": "Lorem ipsum. Lorem ipsum. Lorem ipsum. Lorem ipsum.",
+            "Description": "I started researching RFID packets under Prof. "
+                           "Alanson Sample. RFID are these cool and cheap "
+                           "stickers that reflect waves to a sensor. I'm "
+                           "building an GUI interface that non technical "
+                           "people like doctors can use to examine patients "
+                           "efficiently!",
             "Skills": ["Python"],
-            "Demo": "Lorem ipsum",
-            "Source": "Lorem ipsum",
+            "Demo": "https://www.youtube.com/watch?v=kEkylP-edp8",
+            "Source": "https://github.com/pangr-qwns/kivy_research",
             "Image": "./static/instagram.jpg"
         },
         {
