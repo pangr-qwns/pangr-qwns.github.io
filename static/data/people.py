7181588efbea04e35ff37d4bbdfa12a3eaf6769d
pangr <pangr@umich.edu>
2020-06-08 03:00:56 -0400

    2015 victims

--- static/data/people.py
+++ static/data/people.py
@@ -38,7 +38,7 @@ victims = {
             "Date_of_Incident": "March 13, 2020",
             "Picture": "static/pictures/breonna_taylor.jpg",
             "Link": "https://en.wikipedia.org/wiki/Shooting_of_Breonna_Taylor",
-            "Description": ["Destined for Greatness", "All you can do every day is wake, pray, and slay."]
+            "Description": ["All you can do every day is wake, pray, and slay."]
         },
         {
             "Name": "John Crawford III",
@@ -46,7 +46,7 @@ victims = {
             "Date_of_Incident": "August 5, 2014",
             "Picture": "static/pictures/john_crawford_iii.jpg",
             "Link": "https://en.wikipedia.org/wiki/Shooting_of_John_Crawford_III",
-            "Description": ["How does somebody go into a Walmart and not come out alive?"]
+            "Description": ["How does one go into a Walmart and not come out alive?"]
         },
         {
             "Name": "Ezell Ford",
@@ -62,7 +62,7 @@ victims = {
             "Date_of_Incident": "October 20, 2014",
             "Picture": "static/pictures/laquan_mcdonald.jpg",
             "Link": "https://en.wikipedia.org/wiki/Murder_of_Laquan_McDonald",
-            "Description": ["Bon Bon", "Always bubbly, always smiling, always asking questions"]
+            "Description": ["Bon Bon", "Always bubbly, always smiling"]
         },
         {
             "Name": "Dontre Hamilton",
@@ -70,7 +70,7 @@ victims = {
             "Date_of_Incident": "April 30, 2014",
             "Picture": "static/pictures/dontre_hamilton.jpg",
             "Link": "https://en.wikipedia.org/wiki/Shooting_of_Dontre_Hamilton",
-            "Description": ["We can't bring Dontre back but we can help somebody along the way."]
+            "Description": ["We can't bring him back but we can help somebody."]
         },
         {
             "Name": "Tamir Rice",
@@ -78,7 +78,7 @@ victims = {
             "Date_of_Incident": "November 22, 2014",
             "Picture": "static/pictures/tamir_rice.jpg",
             "Link": "https://en.wikipedia.org/wiki/Shooting_of_Tamir_Rice",
-            "Description": ["A 12-year-old child lost their life, and that should not have happened"]
+            "Description": ["A child lost their life, that should not have happened"]
         },
         {
             "Name": "Antonio Martin",
@@ -103,6 +103,102 @@ victims = {
             "Picture": "static/pictures/alton_sterling.jpg",
             "Link": "https://en.wikipedia.org/wiki/Shooting_of_Alton_Sterling",
             "Description": ["The Most Generous Man in the Neighborhood"]
+        },
+        {
+            "Name": "Charley Leundeu Keunang",
+            "Injury": "Murdered",
+            "Date_of_Incident": "March 1, 2015",
+            "Picture": "static/pictures/charley_lendeu_keunang.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Charley_Leundeu_Keunang",
+            "Description": ["Cameroon", "Physicist"]
+        },
+        {
+            "Name": "Tony Robinson",
+            "Injury": "Murdered",
+            "Date_of_Incident": "March 6, 2015",
+            "Picture": "static/pictures/tony_robinson.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Tony_Robinson",
+            "Description": ["Grandson", "Nephew"]
+        },
+        {
+            "Name": "Anthony Hill",
+            "Injury": "Murdered",
+            "Date_of_Incident": "March 9, 2015",
+            "Picture": "static/pictures/anthony_hill.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Anthony_Hill",
+            "Description": ["Decorated Veteran, US Air Force"]
+        },
+        {
+            "Name": "Meagan Hockaday",
+            "Injury": "Murdered",
+            "Date_of_Incident": "March 28, 2015",
+            "Picture": "static/pictures/meagan_hockaday.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Meagan_Hockaday",
+            "Description": ["Mother of three"]
+        },
+        {
+            "Name": "Walter Scott",
+            "Injury": "Murdered",
+            "Date_of_Incident": "April 4, 2015",
+            "Picture": "static/pictures/walter_scott.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Walter_Scott",
+            "Description": ["Kind Father of four", "Dallas Cowboys Fan"]
+        },
+        {
+            "Name": "Freddie Gray",
+            "Injury": "Murdered",
+            "Date_of_Incident": "April 12, 2015",
+            "Picture": "static/pictures/freddie_gray.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Death_of_Freddie_Gray",
+            "Description": ["Loyal", "Kindhearted", "Warm"]
+        },
+        {
+            "Name": "Jonathan Sanders",
+            "Injury": "Murdered",
+            "Date_of_Incident": "July 8, 2015",
+            "Picture": "static/pictures/jonathan_sanders.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Death_of_Jonathan_Sanders",
+            "Description": ["Complain about a white officer, nothing gets done."]
+        },
+        {
+            "Name": "Sandra Bland",
+            "Injury": "Murdered",
+            "Date_of_Incident": "July 13, 2015",
+            "Picture": "static/pictures/sandra_bland.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Death_of_Sandra_Bland",
+            "Description": ["Activist", "We are fighting to be seen as human."]
+        },
+        {
+            "Name": "Samuel DuBose",
+            "Injury": "Murdered",
+            "Date_of_Incident": "July 19, 2015",
+            "Picture": "static/pictures/samuel_dubose.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Samuel_DuBose",
+            "Description": ["Flesh is just that. Your spirit still lives."]
+        },
+        {
+            "Name": "Jeremy McDole",
+            "Injury": "Murdered",
+            "Date_of_Incident": "September 23, 2015",
+            "Picture": "static/pictures/jeremy_mcdole.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Jeremy_McDole",
+            "Description": ["My world is still dark, but I see today I don't stand alone."]
+        },
+        {
+            "Name": "Corey Jones",
+            "Injury": "Murdered",
+            "Date_of_Incident": "October 18, 2015",
+            "Picture": "static/pictures/corey_jones.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Corey_Jones",
+            "Description": ["Mild Mannered", "Always Smiling"]
+        },
+        {
+            "Name": "Jamar Clark",
+            "Injury": "Murdered",
+            "Date_of_Incident": "November 15, 2015",
+            "Picture": "static/pictures/jamar_clark.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Jamar_Clark",
+            "Description": ["It shows you, in the US, we don’t really have justice."]
         },
     ]
 }
