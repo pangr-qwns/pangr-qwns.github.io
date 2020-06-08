742d30336186fce9eaf594cdce810019f760f678
pangr <pangr@umich.edu>
2020-06-07 23:44:18 -0400

    more victims...

--- static/data/people.py
+++ static/data/people.py
@@ -39,6 +39,70 @@ victims = {
             "Picture": "static/pictures/breonna_taylor.jpg",
             "Link": "https://en.wikipedia.org/wiki/Shooting_of_Breonna_Taylor",
             "Description": ["Destined for Greatness", "All you can do every day is wake, pray, and slay."]
-        }
+        },
+        {
+            "Name": "John Crawford III",
+            "Injury": "Murdered",
+            "Date_of_Incident": "August 5, 2014",
+            "Picture": "static/pictures/john_crawford_iii.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_John_Crawford_III",
+            "Description": ["How does somebody go into a Walmart and not come out alive?"]
+        },
+        {
+            "Name": "Ezell Ford",
+            "Injury": "Murdered",
+            "Date_of_Incident": "August 11, 2014",
+            "Picture": "static/pictures/ezell_ford.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Ezell_Ford",
+            "Description": []
+        },
+        {
+            "Name": "Laquan McDonald",
+            "Injury": "Murdered",
+            "Date_of_Incident": "October 20, 2014",
+            "Picture": "static/pictures/laquan_mcdonald.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Murder_of_Laquan_McDonald",
+            "Description": ["Bon Bon", "Always bubbly, always smiling, always asking questions"]
+        },
+        {
+            "Name": "Dontre Hamilton",
+            "Injury": "Murdered",
+            "Date_of_Incident": "April 30, 2014",
+            "Picture": "static/pictures/dontre_hamilton.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Dontre_Hamilton",
+            "Description": ["We can't bring Dontre back but we can help somebody along the way."]
+        },
+        {
+            "Name": "Tamir Rice",
+            "Injury": "Murdered",
+            "Date_of_Incident": "November 22, 2014",
+            "Picture": "static/pictures/tamir_rice.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Tamir_Rice",
+            "Description": ["A 12-year-old child lost their life, and that should not have happened"]
+        },
+        {
+            "Name": "Antonio Martin",
+            "Injury": "Murdered",
+            "Date_of_Incident": "December 23, 2014",
+            "Picture": "static/pictures/antonio_martin.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Antonio_Martin",
+            "Description": []
+        },
+        {
+            "Name": "Jerame Reid",
+            "Injury": "Murdered",
+            "Date_of_Incident": "December 30, 2014",
+            "Picture": "static/pictures/jerame_reid.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Jerame_Reid",
+            "Description": ["The city has been so gloomy since he died."]
+        },
+        {
+            "Name": "Alton Sterling",
+            "Injury": "Murdered",
+            "Date_of_Incident": "July 5, 2016",
+            "Picture": "static/pictures/alton_sterling.jpg",
+            "Link": "https://en.wikipedia.org/wiki/Shooting_of_Alton_Sterling",
+            "Description": ["The Most Generous Man in the Neighborhood"]
+        },
     ]
 }
