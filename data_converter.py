039398c77484e3d5e8ac48692ea9c6b69a747d9f
pangr <pangr@umich.edu>
2020-06-10 01:59:10 -0400

    added photography page, organized css into files

--- data_converter.py
+++ data_converter.py
@@ -1,6 +1,7 @@
 import json
 from static.data.people import victims
 from static.data.projects import data
+from static.data.photography import pictures
 
 
 if __name__ == "__main__":
@@ -9,3 +10,6 @@ if __name__ == "__main__":
 
     with open("./static/data/people.json", "w", encoding="utf-8") as p:
         json.dump(victims, p, ensure_ascii=False, indent=4)
+
+    with open("./static/data/photography.json", "w", encoding="utf-8") as pics:
+        json.dump(pictures, pics, ensure_ascii=False, indent=4)
